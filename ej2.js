1c106368453803e4a7225898a964f241cbe7e2fa
BryanDZV <bryan.dweb@gmail.com>
2025-09-24 14:02:56 +0200

    ejreiccios

---
+++ ej2.js
@@ -0,0 +1,3 @@
+let nombre=window.prompt("tu nombre");
+let apellido=window.prompt("tu apellido");
+alert(`Bienvenid@ a nuestra página ${nombre} ${apellido} !`)
---
+++ ejercicio1.js
@@ -0,0 +1,10 @@
+/*Vamos a trabajar un poco con la consola para ir amigandonos con ella ya que será nuestra mejor amiga de ahora en más.
+
+Declara una variable llamada nombre en tu consola y asignale tu nombre.
+Declara una variable llamada apellido y asignale como valor tu apellido.
+Muestra por consola las variables nombre y apellido.*/
+
+
+let nombre="bryan";
+let apellido="zv";
+console.log(nombre,apellido);
